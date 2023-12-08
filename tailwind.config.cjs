107a5820b78c925d4c3d75ad405645d62a999384
Glasc <63518997+Glasc@users.noreply.github.com>
2023-12-08 14:42:36 -0600

    fixed dark theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,19 +8,20 @@ module.exports = {
   daisyui: {
     logs: false,
     themes: [
-      {
-        "catppuccin-mocha": {
-          primary: "#89b4fa", // blue
-          secondary: "#f5c2e7", // pink
-          accent: "#94e2d5", // teal
-          neutral: "#deddf4", // crust
-          "base-100": "#1e1e2e", // base
-          info: "#74c7ec", // sapphire
-          success: "#a6e3a1", // green
-          warning: "#f9e2af", // yellow
-          error: "#f38ba8", // red
-        },
-      },
+      "sunset",
+      // {
+      //   "catppuccin-mocha": {
+      //     primary: "#89b4fa", // blue
+      //     secondary: "#f5c2e7", // pink
+      //     accent: "#94e2d5", // teal
+      //     neutral: "#deddf4", // crust
+      //     "base-100": "#1e1e2e", // base
+      //     info: "#74c7ec", // sapphire
+      //     success: "#a6e3a1", // green
+      //     warning: "#f9e2af", // yellow
+      //     error: "#f38ba8", // red
+      //   },
+      // },
       "winter",
     ],
   },
